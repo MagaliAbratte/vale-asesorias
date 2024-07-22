a6558e567b603312adac57c642edae305e0e4bbd
Magali Abratte <maguiabratte@gmail.com>
2024-07-22 20:56:17 -0300

    avance

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,24 +2,34 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { NavLink } from 'react-router-dom';
+import logo from '../assets/home/logo.jpg'
+import logo1 from '../assets/home/logo1.jpg'
 
 export const NavBar = () => {
   return (
-    <div>
-      <Navbar expand="lg" className='nav-total'>
-        <Container className='nav-container'>
-          <NavLink to={'/'} className='title-navbar'>VALENTINA ABALLAY</NavLink>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-toggler-icon"/>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <NavLink to={'./servicios'} className='link-navbar'>Servicios</NavLink>
-              <NavLink to={'./sobremi'}  className='link-navbar'>Sobre Mi</NavLink>
-              <NavLink to={'./contacto'}  className='link-navbar'>Contacto</NavLink>
+    <>
+      <Navbar expand="lg" bg="black" variant="dark" className="custom-navbar">
+        <Container className="d-flex flex-column align-items-start justify-content-between">
+          <div className="d-flex align-items-center justify-content-between w-100">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" className="d-lg-none" />
+            <Navbar.Brand href="/" className="align-items-center mx-auto mx-lg-0">
+              <img 
+                src={logo1} 
+                alt="Logo" 
+                className="d-inline-block align-top img-fluid custom-logo" 
+              />
+            </Navbar.Brand>
+          </div>
+          <Navbar.Collapse id="basic-navbar-nav" className="w-100">
+            <Nav className="w-100 flex-column flex-lg-row align-items-lg-center justify-content-lg-center mt-3 mt-lg-0 text-center">
+              <Nav.Link href="/servicios" className="mx-lg-2">Servicios</Nav.Link>
+              <Nav.Link href="/sobremi" className="mx-lg-2">Sobre Mi</Nav.Link>
+              <Nav.Link href="/contacto" className="mx-lg-2">Contacto</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    </div>
+    </>
   )
 }
 
